990a4a52dd60af801f9c711e8b8b90194582faf3
Jonas Holgersson <jonas.holgersson@sogeti.se>
2011-09-19 14:59:04 +0200

    bootstrapped some development data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,14 @@
+import grails.util.Environment
+import se.lagrummet.Page
+
 class BootStrap {
 
     def init = { servletContext ->
+		
+		if(Environment.current == Environment.DEVELOPMENT) {
+			new Page(title: 'Kalles sida', permaLink: 'kalle', h1: 'heja kalle!').save()
+			new Page(title: 'Lagrummet', permaLink: 'startPage', h1: 'välkommen till lagrummet.se').save()
+		}
     }
     def destroy = {
     }
