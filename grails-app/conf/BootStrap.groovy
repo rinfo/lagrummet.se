1d097b11fd2fdd54855ff603d56367be4e7a9eb7
Jonas Holgersson <jonas.holgersson@sogeti.se>
2011-09-20 16:56:48 +0200

    Bootstrapping data for development

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,5 +1,9 @@
 import grails.util.Environment
 import se.lagrummet.Page
+import se.lagrummet.SecRole
+import se.lagrummet.SecUserSecRole
+import se.lagrummet.User
+
 
 class BootStrap {
 
@@ -8,8 +12,14 @@ class BootStrap {
 		if(Environment.current == Environment.DEVELOPMENT) {
 			new Page(title: 'Kalles sida', permaLink: 'kalle', h1: 'heja kalle!').save()
 			new Page(title: 'Lagrummet', permaLink: 'startPage', h1: 'välkommen till lagrummet.se').save()
+			
+			def user = new User(fullName: 'Jonas', username: 'jonas', password:'password', enabled: true).save(failOnError:true)
+			def roleAdmin = new SecRole(authority: 'ROLE_ADMIN').save(failOnError:true)
+			def roleEditor = new SecRole(authority: 'ROLE_EDITOR').save(failOnError:true)
+			SecUserSecRole.create(user, roleAdmin, true)
 		}
     }
+	
     def destroy = {
     }
 }
