16608a23962b1622138373abc041a0214098df0d
Jonas Holgersson <jonas.holgersson@sogeti.se>
2011-10-21 17:14:53 +0200

    error handling for 404s on the rdl doc page

--- grails-app/controllers/se/lagrummet/RinfoController.groovy
+++ grails-app/controllers/se/lagrummet/RinfoController.groovy
@@ -1,8 +1,10 @@
 package se.lagrummet
 
 
-import groovyx.net.http.*
 import static groovyx.net.http.ContentType.JSON
+import groovyx.net.http.*
+
+import javax.servlet.http.HttpServletResponse
 
 class RinfoController {
 	
@@ -13,21 +15,24 @@ class RinfoController {
 		page.parent = null
 
 		def docPath = "/" + params.docPath
-
-		def docInfo = rinfoService.getDocumentMetaData(docPath)
-
-		def docEntry = rinfoService.getAtomEntry(docPath)
-		
-		def docContent = null
-		docEntry.link.each{ link ->
-			if(link?.@type == "text/html") {
-				docContent = rinfoService.getHtmlContent(docPath)
+		try {
+			def docInfo = rinfoService.getDocumentMetaData(docPath)
+	
+			def docEntry = rinfoService.getAtomEntry(docPath)
+			
+			def docContent = null
+			docEntry.link.each{ link ->
+				if(link?.@type == "text/html") {
+					docContent = rinfoService.getHtmlContent(docPath)
+				}
 			}
+			
+			render(view:'show', model: [page: page, 
+										docInfo: docInfo, 
+										content: docContent, 
+										docEntry: docEntry])
+		} catch (HttpResponseException e) {
+			response.sendError(HttpServletResponse.SC_NOT_FOUND, message(code:"error.rinfodocnotfound"))
 		}
-		
-		render(view:'show', model: [page: page, 
-									docInfo: docInfo, 
-									content: docContent, 
-									docEntry: docEntry])
 	}
 }
