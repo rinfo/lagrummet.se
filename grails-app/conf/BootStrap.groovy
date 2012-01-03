cb391152c90eb68bf9033cc6c504ae6cee80a6df
Jonas Holgersson <jonas.holgersson@sogeti.se>
2012-01-03 11:01:22 +0100

    #41 textfix for search categories

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -64,7 +64,7 @@ class BootStrap {
 			<ul class="learnMore">
 				<li class="heading">L&auml;r dig mer</li>
 				<li><a href="">Om r&auml;ttsinformation</a></li>
-			</ul>''', searchCats: ["Alla","Lagar","Foreskrifter","Rattsfall","Propositioner","Utredningar","Ovrigt"]).save()
+			</ul>''', searchCats: ["Alla","Lagar","Rattsfall","Propositioner","Utredningar","Foreskrifter","Ovrigt"]).save()
 		} else {
 			def user = User.findByUsername("admin") ?: new User(
 				fullName: 'System Admin',
@@ -81,7 +81,7 @@ class BootStrap {
 			def sProps = SiteProperties.findByTitle('lagrummet.se') ?: new SiteProperties(
 				title: "lagrummet.se",
 				siteTitle: 'lagrummet<span class="hlight">.se</span>',
-				searchCats: ["Alla","Lagar","Foreskrifter","Rattsfall","Propositioner","Utredningar","Ovrigt"],
+				searchCats: ["Alla","Lagar","Rattsfall","Propositioner","Utredningar","Foreskrifter","Ovrigt"],
 				headerNavigation: '''
 				<li><a href="">Other languages</a></li>
 				<li><a href="/om-lagrummet-se">Om lagrummet.se</a></li>
