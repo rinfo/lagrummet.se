c4495697bfda63bb93f20e623e08c8356d7a749c
Jonas Holgersson <jonas.holgersson@sogeti.se>
2012-02-10 14:39:40 +0100

    added missing security constraints

--- grails-app/controllers/se/lagrummet/PuffController.groovy
+++ grails-app/controllers/se/lagrummet/PuffController.groovy
@@ -1,5 +1,8 @@
 package se.lagrummet
 
+import grails.plugins.springsecurity.Secured
+
+@Secured(['ROLE_ADMIN', 'IS_AUTHENTICATED_FULLY'])
 class PuffController {
 
     static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
