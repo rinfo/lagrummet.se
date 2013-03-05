83c098f6033b3452fea79d2848ab8db402a02a10
Mabruk Chowdhury <mabruk.c@gmail.com>
2013-03-05 16:20:19 +0100

    Added my ip

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -95,7 +95,7 @@ log4j = {
     //appenders {
     //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
     //}
-
+    
     error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
            'org.codehaus.groovy.grails.web.pages', //  GSP
            'org.codehaus.groovy.grails.web.sitemesh', //  layouts
@@ -123,7 +123,7 @@ grails.plugins.springsecurity.authority.className = 'se.lagrummet.SecRole'
 grails.plugins.springsecurity.ipRestrictions = [
 	'/admin/**': [
 		'159.190.240.1', '159.190.240.2', '159.190.240.3', '159.190.240.254', '159.190.251.57',		//DV Jönköping
-		'159.190.240.66', '159.190.251.56',											//DV Stockholm
+		'159.190.240.66', '159.190.251.56', '159.190.240.42',											//DV Stockholm
 		'159.190.240.150', '159.190.240.151',										//DV Malmö
 		'159.190.240.168', '159.190.240.169', '159.190.240.170', '159.190.240.171',	//DV Göteborg
 		'193.45.43.33',																//Sogeti VPN for development and testing, TODO: remove before going live
