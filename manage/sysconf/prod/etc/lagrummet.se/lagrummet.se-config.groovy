23001df12bd27c7886b001a226e6c729230c0101
Niklas Lind <niklas.lind@valtech.se>
2013-03-28 11:00:22 +0100

    use prod service.lagrummet.se/

--- manage/sysconf/prod/etc/lagrummet.se/lagrummet.se-config.groovy
+++ manage/sysconf/prod/etc/lagrummet.se/lagrummet.se-config.groovy
@@ -1,8 +1,8 @@
 grails.serverURL = "http://localhost:8090/lagrummet.se/"
 lagrummet.local.rinfo.view = "${grails.serverURL}/rinfo/"
 
-lagrummet.rdl.service.baseurl="http://service.demo.lagrummet.se/"
-lagrummet.rdl.rinfo.baseurl="http://rinfo.demo.lagrummet.se/"
+lagrummet.rdl.service.baseurl="http://service.lagrummet.se/"
+lagrummet.rdl.rinfo.baseurl="http://rinfo.lagrummet.se/"
 
 dataSource.url = "jdbc:mysql://localhost:3306/lagrummet"
 dataSource.username = "lagrummet2"
