453e5504a7eff37ddf993e77397a51988c1355ed
cheijken <christian.heijkenskjold@gmail.com>
2014-03-03 08:59:00 +0100

    Fix ÅÄÖ content from RDL

--- grails-app/controllers/se/lagrummet/RinfoController.groovy
+++ grails-app/controllers/se/lagrummet/RinfoController.groovy
@@ -1,5 +1,6 @@
 package se.lagrummet
 
+import groovy.time.TimeCategory
 
 import static groovyx.net.http.ContentType.JSON
 import groovyx.net.http.*
@@ -15,30 +16,58 @@ class RinfoController {
 		def page = [:]
 		page.parent = null
 
+        Date timeStart = new Date();
+
 		def docPath = "/" + params.docPath
 		try {
-			def docInfo = rinfoService.getDocumentMetaData(docPath)
-	
-			def docEntry = rinfoService.getAtomEntry(docPath)
-			
-			def docContent = null
+
+			def docInfo = rinfoService.getDocumentMetaData(docPath); Date timeDocInfo = new Date();
+
+			def docEntry = rinfoService.getAtomEntry(docPath); Date timeDocEntry = new Date();
+
+            def docContent = null
+
 			docEntry.link.each{ link ->
-				if(link?.@type == "text/html") {
+                println "docEntry.link.each "+link
+				if(link?.@type=="text/html") {
 					docContent = rinfoService.getHtmlContent(docPath)
 				}
+                if(link?.@type=="application/xhtml+xml") {
+                    docContent = rinfoService.getXHtmlContent(docPath)
+                }
 			}
-			docEntry.content?.each { content -> 
-				if(content?.@type == "text/html") {
+
+            Date timeLinks = new Date();
+			docEntry.content?.each { content ->
+				if(content?.@type=="text/html") {
 					docContent = rinfoService.getHtmlContent(docPath)
 				}
+                if(content?.@type=="application/xhtml+xml") {
+                    //docContent = rinfoService.getXHtmlContent(docPath)
+                    docContent = rinfoService.getXHtmlContent(content.@src.text())
+                }
 			}
+
+            Date timeContents = new Date();
 			if(docContent != null) {
 				docContent = htmlSanitizerService.cleanHtml(docContent)
 			}
-			render(view:'show', model: [page: page, 
+            Date timeSanitizer = new Date();
+
+			render(view:'show', model: [page: page,
 										docInfo: docInfo, 
 										content: docContent, 
 										docEntry: docEntry])
+            Date timeRender = new Date();
+
+            println "------------ Preformance eval of "+docPath+" ------------------------"
+            println "rinfoService.getDocumentMetaData(docPath): "+TimeCategory.minus(timeDocInfo, timeStart)
+            println "rinfoService.getAtomEntry(docPath): "+TimeCategory.minus(timeDocEntry, timeStart)
+            println "rinfoService.getHtmlContent(docPath): "+TimeCategory.minus(timeLinks, timeStart)
+            println "rinfoService.rinfoService.getHtmlContent(docPath): "+TimeCategory.minus(timeContents, timeStart)
+            println "rinfoService.rinfoService.getHtmlContent(Sanitizer): "+TimeCategory.minus(timeSanitizer, timeStart)
+            println "render: "+TimeCategory.minus(timeRender, timeStart)
+            println "------------ Completed ------------------------------------------"
 		} catch (HttpResponseException e) {
 			forward(controller: "page", action: "error", params: [errorId: "404"])
 		}
