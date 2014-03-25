f55bed2c8730cdc8a8ac6602ae84da149dc9de1a
Mattias Persson <mattias.persson@valtech.se>
2014-03-25 15:30:46 +0100

    Update varnish route for test environment

--- manage/sysconf/test/etc/lagrummet.se/lagrummet.se-config.groovy
+++ manage/sysconf/test/etc/lagrummet.se/lagrummet.se-config.groovy
@@ -4,7 +4,7 @@ grails.serverURL = "http://test.lagrummet.se"
 lagrummet.local.rinfo.view = "${grails.serverURL}/rinfo/"
 
 // Route rinfo-service requests through varnish
-lagrummet.rdl.service.baseurl="http://127.0.0.1:8383/rinfo-service/"
+lagrummet.rdl.service.baseurl="http://127.0.0.1:8383/"
 
 //lagrummet.rdl.service.baseurl="http://service.test.lagrummet.se/"
 lagrummet.rdl.rinfo.baseurl="http://rinfo.test.lagrummet.se/"
