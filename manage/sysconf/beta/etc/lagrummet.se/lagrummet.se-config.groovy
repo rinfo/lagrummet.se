fbd8c3831d2012c5aa2222474038acd07348201c
Jon Eldeklint <jon@eldeklint.se>
2014-04-22 15:03:39 +0200

    Using hostname for varnish instead of local-ip
    to make it work with a distributed setup. #103

--- manage/sysconf/beta/etc/lagrummet.se/lagrummet.se-config.groovy
+++ manage/sysconf/beta/etc/lagrummet.se/lagrummet.se-config.groovy
@@ -2,7 +2,7 @@ grails.serverURL = "http://beta.lagrummet.se"
 lagrummet.local.rinfo.view = "${grails.serverURL}/rinfo/"
 
 // Route rinfo-service requests through varnish
-lagrummet.rdl.service.baseurl="http://127.0.0.1:8383/"
+lagrummet.rdl.service.baseurl="http://rinfo.beta.lagrummet.se:8383/"
 
 //lagrummet.rdl.service.baseurl="http://service.beta.lagrummet.se/"
 lagrummet.rdl.rinfo.baseurl="http://rinfo.beta.lagrummet.se/"
