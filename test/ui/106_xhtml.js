e26255bb6e526a2081771853a85f5b882a028bc6
cheijken <christian.heijkenskjold@gmail.com>
2014-04-25 11:52:13 +0200

    Test to verify issue #106

---
+++ test/ui/106_xhtml.js
@@ -0,0 +1,28 @@
+var x = require('casper').selectXPath;
+casper.on('page.error', function(msg, trace) {
+   this.echo('Error: ' + msg, 'ERROR');
+   for(var i=0; i<trace.length; i++) {
+       var step = trace[i];
+       this.echo('   ' + step.file + ' (line ' + step.line + ')', 'ERROR');
+   }
+});
+casper.test.begin('Test rendering of xhtml content with swedish characters', function(test) {
+   casper.start('http://demo.lagrummet.se/rinfo/publ/sfs/1999:175/konsolidering/2011-05-02');
+   casper.waitForSelector("body",
+       function success() {
+           test.assertExists("body");
+           this.click("body");
+       },
+       function fail() {
+           test.assertExists("body");
+   });
+   casper.waitForSelector(x("//*[contains(text(), \'Rättsinformationsförordning (1999:175)\')]"),
+       function success() {
+           test.assertExists(x("//*[contains(text(), \'Rättsinformationsförordning (1999:175)\')]"));
+         },
+       function fail() {
+           test.assertExists(x("//*[contains(text(), \'Rättsinformationsförordning (1999:175)\')]"));
+   });
+
+   casper.run(function() {test.done();});
+});
