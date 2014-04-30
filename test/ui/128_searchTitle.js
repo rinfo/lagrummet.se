aa3bb860b59d7890fb70c35ef3621f1ffd22b4b6
Jon Eldeklint <jon@eldeklint.se>
2014-04-30 12:14:05 +0200

    Simple ui test for title in search results
    right now it tests test.lagrummet.se, should be parameterized. tests #128

---
+++ test/ui/128_searchTitle.js
@@ -0,0 +1,39 @@
+/*==============================================================================*/
+/* Casper generated Wed Apr 30 2014 12:06:20 GMT+0200 (CEST) */
+/*==============================================================================*/
+
+var x = require('casper').selectXPath;
+casper.on('page.error', function(msg, trace) {
+    this.echo('Error: ' + msg, 'ERROR');
+    for(var i=0; i<trace.length; i++) {
+        var step = trace[i];
+        this.echo('   ' + step.file + ' (line ' + step.line + ')', 'ERROR');
+    }
+});
+casper.test.begin('When searching for \'Lagar\', result title should be the title of the \'Lag\'', function(test) {
+    casper.start('http://test.lagrummet.se/');
+    casper.waitForSelector("form[name=search] input[name='query']",
+        function success() {
+            test.assertExists("form[name=search] input[name='query']");
+            this.click("form[name=search] input[name='query']");
+        },
+        function fail() {
+            test.assertExists("form[name=search] input[name='query']");
+        });
+    casper.waitForSelector("input[name='query']",
+        function success() {
+            this.sendKeys("input[name='query']", "studiemedel");
+        },
+        function fail() {
+            test.assertExists("input[name='query']");
+        });
+    casper.waitForSelector(x("//a[normalize-space(text())='Förordning (2005:55) om elektronisk överföring av ansökningar om studiemedel']"),
+        function success() {
+            test.assertExists(x("//a[normalize-space(text())='Förordning (2005:55) om elektronisk överföring av ansökningar om studiemedel']"));
+        },
+        function fail() {
+            test.assertExists(x("//a[normalize-space(text())='Förordning (2005:55) om elektronisk överföring av ansökningar om studiemedel']"));
+        });
+
+    casper.run(function() {test.done();});
+});
