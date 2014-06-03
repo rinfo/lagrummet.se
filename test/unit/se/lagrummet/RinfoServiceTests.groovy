9166d63a204b4ae792fe33c2443998302d3bd3e8
Jon Eldeklint <jon@eldeklint.se>
2014-06-03 13:55:24 +0200

    using stream.getBytes, to avoid content truncation
    Fixes #136
    Previously only the first 100K bytes where read from documents in rdl.
    By using stream.getBytes instead of the homemade byte-reader we get the
    whole document, even if it's the BrB..
    
    Added a somewhat lousy test to make sure the document isn't truncated.

--- test/unit/se/lagrummet/RinfoServiceTests.groovy
+++ test/unit/se/lagrummet/RinfoServiceTests.groovy
@@ -53,4 +53,25 @@ class RinfoServiceTests extends GrailsUnitTestCase {
 
         assertFalse xhtml == inputString
     }
+
+    void testShouldBeAbleToReadALongDocument() {
+        def final inputStringTop = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML+RDFa 1.0//EN\"\n" +
+                "    \"http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd\">\n" +
+                "<html xmlns=\"http://www.w3.org/1999/xhtml\"\n" +
+                "    xmlns:foaf=\"http://xmlns.com/foaf/0.1/\"\n" +
+                "    xmlns:dc=\"http://purl.org/dc/elements/1.1/\"\n" +
+                "    version=\"XHTML+RDFa 1.0\" xml:lang=\"en\">\n" +
+                "  <head>\n" +
+                "    <title></title>\n" +
+                "  </head>\n";
+        def final inputStringBottom = "</html>";
+
+        def inputString = inputStringTop.padRight(150000 , 'A')
+        inputString = inputString.concat(inputStringBottom)
+        def service = new RinfoService()
+        def xhtml = service.getXHtmlContentJavaVersion(new ByteArrayInputStream(inputString.getBytes()))
+
+        assertTrue(xhtml.length() >= inputString.length())
+    }
 }
