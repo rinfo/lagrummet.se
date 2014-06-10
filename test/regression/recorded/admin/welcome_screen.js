cfda3547c5ba44859dcae69f3bc498135ca366c5
Daniel By <daniel.by@sogeti.se>
2014-06-10 14:32:27 +0200

    Removed not needed code in welcome page test

--- test/regression/recorded/admin/welcome_screen.js
+++ test/regression/recorded/admin/welcome_screen.js
@@ -7,63 +7,17 @@ casper.on('page.error', function(msg, trace) {
         this.echo('   ' + step.file + ' (line ' + step.line + ')', 'ERROR');
     }
 });
-casper.test.begin('Välkomstsida', function(test) {
-    casper.start(casper.cli.get("url"));
-    casper.waitForSelector(x("//*[contains(text(), \'Välkommen till lagrummet.se\')]"),
-        function success() {
-            test.assertExists(x("//*[contains(text(), \'Välkommen till lagrummet.se\')]"));
-        },
-        function fail() {
-            test.assertExists(x("//*[contains(text(), \'Välkommen till lagrummet.se\')]"));
-        });
 
-    casper.waitForSelector(x("//*[contains(text(), \'Prova vår nya sökfunktion\')]"),
-        function success() {
-            test.assertExists(x("//*[contains(text(), \'Prova vår nya sökfunktion\')]"));
-        },
-        function fail() {
-            test.assertExists(x("//*[contains(text(), \'Prova vår nya sökfunktion\')]"));
-        });
-
-    casper.waitForSelector(x("//a[normalize-space(text())='Hitta orden!']"),
-        function success() {
-            test.assertExists(x("//a[normalize-space(text())='Hitta orden!']"));
-        },
-        function fail() {
-            test.assertExists(x("//a[normalize-space(text())='Hitta orden!']"));
-        });
-
-    casper.waitForSelector(x("//a[normalize-space(text())='Ordlista A–Ö']"),
-        function success() {
-            test.assertExists(x("//a[normalize-space(text())='Ordlista A–Ö']"));
-        },
-        function fail() {
-            test.assertExists(x("//a[normalize-space(text())='Ordlista A–Ö']"));
-        });
-
-    casper.waitForSelector(x("//img[contains(@alt, \'Förstoringsglas\')]"),
-        function success() {
-            test.assertExists(x("//img[contains(@alt, \'Förstoringsglas\')]"));
-        },
-        function fail() {
-            test.assertExists(x("//img[contains(@alt, \'Förstoringsglas\')]"));
-        });
-
-    casper.waitForSelector(x("//div[@id='logo']/a[contains(text(), \'lagrummet\')]"),
-        function success() {
-            test.assertExists(x("//div[@id='logo']/a[contains(text(), \'lagrummet\')]"));
-        },
-        function fail() {
-            test.assertExists(x("//div[@id='logo']/a[contains(text(), \'lagrummet\')]"));
-        });
-
-    casper.waitForSelector(x("//div[@id='logo']/a/span[@class='hlight'][contains(text(), \'.se\')]"),
-        function success() {
-            test.assertExists(x("//div[@id='logo']/a/span[@class='hlight'][contains(text(), \'.se\')]"));
-        },
-        function fail() {
-            test.assertExists(x("//div[@id='logo']/a/span[@class='hlight'][contains(text(), \'.se\')]"));
-        });
+casper.test.begin('Välkomstsida', function suite(test) {
+    casper.start(casper.cli.get("url"), function() {
+        test.assertExists(x("//*[contains(text(), \'Välkommen till lagrummet.se\')]"));
+        test.assertExists(x("//*[contains(text(), \'Prova vår nya sökfunktion\')]"));
+        test.assertExists(x("//a[normalize-space(text())='Hitta orden!']"));
+        test.assertExists(x("//a[normalize-space(text())='Ordlista A–Ö']"));
+        test.assertExists(x("//img[contains(@alt, \'Förstoringsglas\')]"));
+        test.assertExists(x("//div[@id='logo']/a[contains(text(), \'lagrummet\')]"));
+        test.assertExists(x("//div[@id='logo']/a/span[@class='hlight'][contains(text(), \'.se\')]"));
+    });
 
     casper.run(function() {test.done();});
 });
