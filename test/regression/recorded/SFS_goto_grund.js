8de9c894cfbbb1fa90a85f5e8358634f60e5f065
cheijken <christian.heijkenskjold@gmail.com>
2014-07-03 11:22:59 +0200

    Fix recorded test. #159

--- test/regression/recorded/SFS_goto_grund.js
+++ test/regression/recorded/SFS_goto_grund.js
@@ -1,5 +1,5 @@
 var x = require('casper').selectXPath;
-casper.options.viewportSize = {width: 1920, height: 1128};
+
 casper.on('page.error', function(msg, trace) {
    this.echo('Error: ' + msg, 'ERROR');
    for(var i=0; i<trace.length; i++) {
@@ -7,38 +7,37 @@ casper.on('page.error', function(msg, trace) {
        this.echo('   ' + step.file + ' (line ' + step.line + ')', 'ERROR');
    }
 });
+
+captureScreen = function() {
+   var file_name = casper.cli.get("output")+'SFS_goto_ground_screen_error.png';
+   this.capture(file_name);
+   this.echo('Captured "'+file_name+'"');
+}
+
 casper.test.begin('Navigera från konsolidering till grund', function(test) {
    casper.start(casper.cli.get("url"));
-   casper.waitForSelector("form[name=search] input[name='query']",
-       function success() {
-           test.assertExists("form[name=search] input[name='query']");
-           this.click("form[name=search] input[name='query']");
-       },
-       function fail() {
-           test.assertExists("form[name=search] input[name='query']");
+
+   casper.waitForSelector("body");
+
+   casper.then(function() {
+        this.test.assertExists("form[name=search] input[name='query']");
+        this.test.assertTextDoesntExist('Lag (1974:981) om arbetstagares rätt till ledighet för utbildning');
+
+        this.sendKeys("input[name='query']", "studieledighetslagen");
    });
-   casper.waitForSelector("input[name='query']",
-       function success() {
-           this.sendKeys("input[name='query']", "studieledighetslagen");
-       },
-       function fail() {
-           test.assertExists("input[name='query']");
+
+   casper.waitForSelector("#dynamicSearchResults > header > h1", function(){}, captureScreen, 20000);
+
+   casper.then(function() {
+        this.test.assertSelectorHasText('#dynamicSearchResults > header > h1','Sökresultat för studieledighetslagen');
+        this.test.assertSelectorHasText('#lagar > li:nth-child(1) > p:nth-child(1) > a','Lag (1974:981) om arbetstagares rätt till ledighet för utbildning');
+        this.click('#lagar > li:nth-child(1) > p:nth-child(1) > a');
    });
-   casper.waitForSelector(x("//a[normalize-space(text())='Lag (1974:981) om arbetstagares rätt till ledighet för utbildning']"),
-       function success() {
-           test.assertExists(x("//a[normalize-space(text())='Lag (1974:981) om arbetstagares rätt till ledighet för utbildning']"));
-           this.click(x("//a[normalize-space(text())='Lag (1974:981) om arbetstagares rätt till ledighet för utbildning']"));
-       },
-       function fail() {
-           test.assertExists(x("//a[normalize-space(text())='Lag (1974:981) om arbetstagares rätt till ledighet för utbildning']"));
-   });
-   casper.waitForSelector(x("//a[normalize-space(text())='Lag (1974:981) om arbetstagares rätt till ledighet för utbildning']"),
-       function success() {
-           test.assertExists(x("//a[normalize-space(text())='Lag (1974:981) om arbetstagares rätt till ledighet för utbildning']"));
-           this.click(x("//a[normalize-space(text())='Lag (1974:981) om arbetstagares rätt till ledighet för utbildning']"));
-       },
-       function fail() {
-           test.assertExists(x("//a[normalize-space(text())='Lag (1974:981) om arbetstagares rätt till ledighet för utbildning']"));
+
+   casper.waitForSelector("#rinfo", function(){}, captureScreen, 20000);
+
+   casper.then(function() {
+        this.test.assertSelectorHasText('#rinfo > h1','Lag (1974:981) om arbetstagares rätt till ledighet för utbildning');
    });
 
    casper.run(function() {test.done();});
