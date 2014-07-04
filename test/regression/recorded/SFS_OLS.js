c34b6533e071b178c73180a81c47ba95029d6ee3
cheijken <christian.heijkenskjold@gmail.com>
2014-07-04 09:30:00 +0200

    Reconstruct recorded test. #159

--- test/regression/recorded/SFS_OLS.js
+++ test/regression/recorded/SFS_OLS.js
@@ -7,30 +7,37 @@ casper.on('page.error', function(msg, trace) {
        this.echo('   ' + step.file + ' (line ' + step.line + ')', 'ERROR');
    }
 });
+
+captureScreen = function() {
+   var file_name = casper.cli.get("output")+'SFS_OLS_screen_error.png';
+   this.capture(file_name);
+   this.echo('Captured "'+file_name+'"');
+}
+
 casper.test.begin('Sök efter OLS', function(test) {
    casper.start(casper.cli.get("url"));
-   casper.waitForSelector("form[name=search] input[name='query']",
-       function success() {
-           test.assertExists("form[name=search] input[name='query']");
-           this.click("form[name=search] input[name='query']");
-       },
-       function fail() {
-           test.assertExists("form[name=search] input[name='query']");
+
+   casper.waitForSelector("body", function(){}, captureScreen, 5000);
+
+   casper.then(function() {
+        this.test.assertExists("form[name=search] input[name='query']");
+        this.test.assertTextDoesntExist('Lag (1974:981) om arbetstagares rätt till ledighet för utbildning');
+
+        this.sendKeys("input[name='query']", "offentlighets och sekretesslag");
    });
-   casper.waitForSelector("input[name='query']",
-       function success() {
-           this.sendKeys("input[name='query']", "offentlighets och sekretesslag");
-       },
-       function fail() {
-           test.assertExists("input[name='query']");
+
+   casper.waitForSelector("#dynamicSearchResults > header > h1", function(){}, captureScreen, 20000);
+
+   casper.then(function() {
+        this.test.assertSelectorHasText('#dynamicSearchResults > header > h1','Sökresultat för offentlighets och sekretesslag');
+        this.test.assertSelectorHasText('#lagar > li:nth-child(1) > p:nth-child(1) > a','Offentlighets- och sekretesslag (2009:400)');
+        this.click('#lagar > li:nth-child(1) > p:nth-child(1) > a');
    });
-   casper.waitForSelector(x("//a[normalize-space(text())='Offentlighets- och sekretesslag (2009:400)']"),
-       function success() {
-           test.assertExists(x("//a[normalize-space(text())='Offentlighets- och sekretesslag (2009:400)']"));
-           this.click(x("//a[normalize-space(text())='Offentlighets- och sekretesslag (2009:400)']"));
-       },
-       function fail() {
-           test.assertExists(x("//a[normalize-space(text())='Offentlighets- och sekretesslag (2009:400)']"));
+
+   casper.waitForSelector("#rinfo", function(){}, captureScreen, 20000);
+
+   casper.then(function() {
+        this.test.assertSelectorHasText('#rinfo > h1','Offentlighets- och sekretesslag (2009:400)');
    });
 
    casper.run(function() {test.done();});
