019a6a954b69a737658b133d6a5edf865632fdcd
cheijken <christian.heijkenskjold@gmail.com>
2014-08-13 16:34:19 +0200

    Add DB create check

--- manage/sysconf/test/mysql/setup-mysql.sql
+++ manage/sysconf/test/mysql/setup-mysql.sql
@@ -1,4 +1,4 @@
-create database lagrummet;
+create database IF NOT EXISTS lagrummet;
 use lagrummet;
 CREATE USER 'lagrummet2'@'localhost' IDENTIFIED BY 'changeme';
 GRANT ALL PRIVILEGES ON *.* TO 'lagrummet2'@'localhost';
