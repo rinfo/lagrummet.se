e42faa4fe71733b070666ac42d571d0e1e5d85b8
cheijken <christian.heijkenskjold@gmail.com>
2014-08-14 09:30:31 +0200

    Reactivate test

--- test/regression/106_xhtml.js
+++ test/regression/106_xhtml.js
@@ -16,7 +16,6 @@ captureScreen = function() {
 casper.test.begin('Test rendering of xhtml content with swedish characters', function(test) {
    casper.start(casper.cli.get("url")+'/rinfo/publ/sfs/1999:175/konsolidering/2011-05-02');
 
-/*
    casper.waitForSelector("body", function(){}, captureScreen, 5000);
 
    casper.then(function() {
@@ -26,7 +25,7 @@ casper.test.begin('Test rendering of xhtml content with swedish characters', fun
 
        this.test.assertSelectorHasText("#rinfo > h1","Rättsinformationsförordning (1999:175)");
    });
-*/
+
 
    casper.run(function() {test.done();});
 });
