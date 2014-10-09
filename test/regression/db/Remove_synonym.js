ee455847c983abee035e5f2a60fd50231e86e5d7
Daniel By <daniel.by@sogeti.se>
2014-10-09 14:51:37 +0200

    #179 Implemented Edit_Source.js and Remove_synonym.js, fixed bug in Add_synonym.js

--- test/regression/db/Remove_synonym.js
+++ test/regression/db/Remove_synonym.js
@@ -1,4 +1,3 @@
-//TODO Complete according to spec
 /*Tabort Synonymen
   	Gör ett test i samband med att issuen för detta görs
   	-i menyn lagrummet - synoymer
@@ -15,91 +14,49 @@ casper.test.begin('Remove synonym', function(test) {
 
     // prepare test
     casper.waitForSelector("body", function(){}, captureScreen, 5000);
-    //casper.then(login);
-    //casper.waitForSelector("#adminPages", function(){}, captureScreen, 5000);
-    //casper.then(verifyLogin);
+    casper.then(login);
+    casper.waitForSelector("#adminPages", function(){}, captureScreen, 5000);
+    casper.then(verifyLogin);
 
-/*
+
     // Test starts here
-   casper.then(function() {
-        this.test.assertSelectorDoesntHaveText('#bodyContent > div > h1','Rättskällor');
-        this.click('#adminFunctions > ul > li:nth-child(4) > ul > li:nth-child(2) > a'); // Click at 'Rättskällor -> Hantera rättskällor'
-   });
-
-   casper.waitForSelector("#bodyContent > div", function(){}, captureScreen, 5000);
-
-   casper.then(function() {
-        this.test.assertSelectorHasText('#bodyContent > div > h1','Rättskällor');
-
-        var clickSelector = this.evaluate(findForarbeteInList);
-
-        this.test.assertSelectorDoesntHaveText('#bodyContent > div > h1','Redigera Rättskälla');
-        if (clickSelector!="")
-            this.click(clickSelector);
-        else
-            casper.test.fail("No click selector!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-   });
-
-   casper.waitForText('Redigera Rättskälla', function(){}, captureScreen, 5000);
-
-   casper.then(function() {
-        this.test.assertSelectorHasText('#bodyContent > div > h1','Redigera Rättskälla');
-
-        this.fill('form#form_edit_source', {
-            'description':    'Testbeskrivning',
-        });
-
-        this.click('#save'); //Click spara
-   });
-
-   casper.waitForText('Rättskälla Förarbete uppdaterad', function(){}, captureScreen, 20000);
-
-   casper.then(function() {
-        this.test.assertSelectorHasText('#bodyContent > div > div','Rättskälla Förarbete uppdaterad');
-        this.click('body > header > a');
-   });
-
-    casper.waitForSelector("#content > article > header > h1", function(){}, captureScreen, 20000);
-
     casper.then(function() {
-        var CSS_PATH_TO_MENU = casper.evaluate(findTextInNthChildMenu,'Förarbeten');
-        if (CSS_PATH_TO_MENU=='')
-            captureScreen();
-        this.test.assertSelectorHasText(CSS_PATH_TO_MENU,'Förarbeten');
-        this.click(CSS_PATH_TO_MENU);
+        this.test.assertSelectorHasText(x('//*[@id="adminFunctions"]/ul/*/ul/*/a'), 'Hantera synonymer');
+        this.click(x('//*[@id="adminFunctions"]/ul/*/ul/*/a[text()="Hantera synonymer"]')); // Click at 'Rättskällor -> Hantera rättskällor'
     });
 
-    casper.waitForSelector("#content > article > header > h1", function(){}, captureScreen, 20000);
+    casper.waitForSelector("#bodyContent > div", function(){}, captureScreen, 5000);
 
-    casper.then(function() {
-        this.test.assertSelectorHasText('#legalSource_subCategory_Regeringen_list > li:nth-child(1) > a','Förarbete');
-        this.test.assertSelectorHasText('#legalSource_subCategory_Regeringen_list > li:nth-child(1) > div','Testbeskrivning');
-    });
+   casper.then(function() {
+       this.test.assertSelectorHasText('#bodyContent > div > h1','Synonymer');
 
-    casper.then(function() {
-        var CSS_PATH_TO_ALL_MENU = casper.evaluate(findTextInNthChildMenu,'Alla rättskällor');
-        if (CSS_PATH_TO_ALL_MENU=='')
-            captureScreen();
-        this.test.assertSelectorHasText(CSS_PATH_TO_ALL_MENU,'Alla rättskällor');
-        this.click(CSS_PATH_TO_ALL_MENU);
-    });
 
-    casper.waitForText("Lista över rättskällorna", function(){}, captureScreen, 20000);
+       this.test.assertExists(x('//*[@id="editSynonyms"]/*/table/tbody/*/td/input[@type="text" and @value="skilsmässa"]'));
+       this.test.assertExists(x('//*[@id="editSynonyms"]/*/table/tbody/*/td/input[@type="text" and @value="äktenskapsskillnad"]'));
 
-    casper.then(function() {
-        this.test.assertSelectorHasText('#content > article > header > h1','Lista över rättskällorna');
-        this.test.assertSelectorHasText('#Forarbeten_sokbar_list > li:nth-child(1) > a','Förarbete');
-    });
-*/
+       this.click(x('//*[@id="editSynonyms"]/*/table/tbody/*/td/input[@type="text" and @value="skilsmässa"]/../../td/div[@class="buttons"]/a'));
+   });
+
+   casper.waitForText('borttagen', function(){}, captureScreen, 5000);
+
+   casper.then(function() {
+       this.test.assertNotExists(x('//*[@id="editSynonyms"]/*/table/tbody/*/td/input[@type="text" and @value="skilsmässa"]'));
+       this.test.assertNotExists(x('//*[@id="editSynonyms"]/*/table/tbody/*/td/input[@type="text" and @value="äktenskapsskillnad"]'));
+   });
+
+   casper.then(logout);
+
+   casper.waitForSelector("#content > article > header > h1", function(){}, captureScreen, 20000);
+
+   casper.then(function() {
+       this.sendKeys("input[name='query']", "skilsmässa");
+   });
+
+   casper.waitForSelector("#dynamicSearchResults > header > h1", function(){}, captureScreen, 20000);
+
+   casper.then(function() {
+       this.test.assertNotExists(x('//*[@id="dynamicSearchResults"]/p/span[text()="äktenskapsskillnad"]'));
+   });
 
    casper.run(function() {test.done();});
 });
-
-var findForarbeteInList = function() {
-    for (n = 1; n <= 20; n++) {
-        var testSelector = "#bodyContent > div > div.list > table > tbody > tr:nth-child("+n+") > td:nth-child(1) > a";
-        if (document.querySelector(testSelector).innerHTML=="Förarbete")
-            return testSelector;
-    }
-    return "";
-}
