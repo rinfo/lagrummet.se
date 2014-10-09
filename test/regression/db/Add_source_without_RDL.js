780390006d1c91173e97c8fbc8df92dcd82b47e7
Daniel By <daniel.by@sogeti.se>
2014-10-09 09:10:26 +0200

    #179 Implementation of test

--- test/regression/db/Add_source_without_RDL.js
+++ test/regression/db/Add_source_without_RDL.js
@@ -19,56 +19,45 @@ casper.test.begin('Add source without rdl', function(test) {
    casper.waitForSelector("#bodyContent > div", function(){}, captureScreen, 5000);
 
    casper.then(function() {
-        this.test.assertSelectorHasText('#bodyContent > div > h1','Skapa Rättskälla');
-// todo fix below tests. Works everywhere but fails when running aganins regression.lagrummet.se in Jenkins demo.lagrummet script
-/*        this.fill('form#form_create_source', {
-            'url':    'http://www.abcmyndigheten.se',
-            'name':    'ABC-myndigheten',
-            'category':       'Foreskrifter',
-        });
+       this.test.assertSelectorHasText('#bodyContent > div > h1','Skapa Rättskälla');
 
-        this.click('#create');*/
+       this.fill('form#form_create_source', {
+           'url':    'http://www.abcmyndigheten.se',
+           'name':    'ABC-myndigheten 2',
+           'category':       'Foreskrifter',
+           'subCategory':      'Regeringen'
+       });
+
+       this.click('#create');
    });
 
-   /*casper.waitForText('Redigera Rättskälla', function(){}, captureScreen, 20000);
+   casper.waitForText('Redigera Rättskälla', function(){}, captureScreen, 20000);
 
    casper.then(function() {
         this.test.assertSelectorHasText('#bodyContent > div > h1','Redigera Rättskälla');
-
-        this.click('body > header > a');
    });
 
-   casper.waitForSelector("#logo > a", function(){}, captureScreen, 20000);
+   casper.then(logout);
 
-   casper.then(function() {
-        var CSS_PATH_TO_MENU = casper.evaluate(findTextInNthChildMenu,'Myndigheters föreskrifter');
-        if (CSS_PATH_TO_MENU=='')
-            this.wait(1,captureScreen);
-        this.test.assertSelectorHasText(CSS_PATH_TO_MENU,'Myndigheters föreskrifter');
-        this.click(CSS_PATH_TO_MENU);
-   });
+   casper.waitForSelector("#content > article > header > h1", function(){}, captureScreen, 20000);
+
+   casper.then(goToMyndighetersForeskrifter);
 
    casper.waitForSelector("#content > article > header > h1", function(){}, captureScreen, 20000);
 
    casper.then(function() {
         this.test.assertSelectorHasText('#content > article > header > h1','Myndigheters föreskrifter');
-        this.test.assertSelectorHasText('#legalSource_category_Foreskrifter_list > li:nth-child(2) > a','ABC-myndigheten');
+        this.test.assertSelectorHasText('#legalSource_category_Foreskrifter_list > li > a','ABC-myndigheten 2');
    });
 
-   casper.then(function() {
-        var CSS_PATH_TO_ALL_MENU = casper.evaluate(findTextInNthChildMenu,'Alla rättskällor');
-        if (CSS_PATH_TO_ALL_MENU=='')
-            this.wait(1,captureScreen);
-        this.test.assertSelectorHasText(CSS_PATH_TO_ALL_MENU,'Alla rättskällor');
-        this.click(CSS_PATH_TO_ALL_MENU);
-   });
+   casper.then(goToAllaRattskallor);
 
    casper.waitForSelector("#content > article > header > h1", function(){}, captureScreen, 20000);
 
    casper.then(function() {
         this.test.assertSelectorHasText('#content > article > header > h1','Lista över rättskällorna');
-        this.test.assertSelectorHasText('#Foreskrifter_inteSokbar_list > li:nth-child(1) > a','ABC-myndigheten');
-   });*/
+        this.test.assertSelectorHasText('#Foreskrifter_inteSokbar_list > li > a','ABC-myndigheten 2');
+   });
 
    casper.run(function() {test.done();});
 });
