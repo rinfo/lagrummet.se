13e52f521b6dafaf8d4c5dc6394f91a90c6f7a72
Kåre Jonsson <kare.jonsson@dom.se>
2014-11-14 15:35:12 +0100

    Adds target dom and ville

---
+++ manage/sysconf/ville/etc/lagrummet.se/lagrummet.se-config.groovy
@@ -0,0 +1,16 @@
+//package sysconf.test.etc.lagrummet.se
+
+grails.serverURL = "http://ville.lagrummet.se"
+lagrummet.local.rinfo.view = "${grails.serverURL}/rinfo/"
+
+// Route rinfo-service requests through varnish
+lagrummet.rdl.service.baseurl="http://127.0.0.1:8383/"
+
+//lagrummet.rdl.service.baseurl="http://service.test.lagrummet.se/"
+lagrummet.rdl.rinfo.baseurl="http://rinfo.ville.lagrummet.se/"
+
+dataSource.url = "jdbc:mysql://127.0.0.1:3306/lagrummet"
+dataSource.username = "lagrummet2"
+dataSource.password = "changeme"
+
+lagrummet.googleAnalytics.webPropertyId = "UA-49599039-2"
