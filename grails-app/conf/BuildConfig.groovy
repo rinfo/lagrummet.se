6c57bd9f56b710d61eb425f4ea01a1da6421c23b
Jon Eldeklint <jon@eldeklint.se>
2014-12-12 16:40:32 +0100

    Uninstall grails-tinyMce plugin #313

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -50,7 +50,6 @@ grails.project.dependency.resolution = {
     }
     plugins {
         compile ":hibernate:3.6.10.17"
-        compile ":tiny-mce:3.4.9"
         compile ":searchable:0.6.9"
         compile ":quartz2:2.1.6.2"
         compile ":jquery:1.11.1"
