90f073c5d0ba8a0d5d96397aee0616207cb021d0
Jon Eldeklint <jon@eldeklint.se>
2014-12-17 11:00:29 +0100

    Makes the toolbar more like before #313

--- web-app/js/admin.js
+++ web-app/js/admin.js
@@ -18,6 +18,7 @@ jQuery(function($) {
 		theme: "modern",
 		forced_root_block : "",
 		force_p_newlines : true,
+		menubar : false,
 		plugins: [
 			"advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
 			"searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
@@ -25,7 +26,7 @@ jQuery(function($) {
 		],
 		image_list: serverUrl + "admin/media/list?ajax=true&parentId=" + pageId,
 		content_css: "../../css/main.css",
-		toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | forecolor backcolor code ",
+		toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | removeformat visualchars | bullist numlist outdent indent | link anchor image | forecolor backcolor | code fullscreen",
 		extended_valid_elements : "nav[class]",
 		entity_encoding : "raw",
 		formats : {
@@ -54,7 +55,7 @@ jQuery(function($) {
 	var initiallyOpen = new Array();
 	$("#pageTree > ul > li, #pageTree ul li.metaPage").each(function(n) {
 		initiallyOpen.push($(this).attr("id"));
-	});	
+	});
 	
 	$("#pageTree li.currentPage").parents("#pageTree li").each(function(n) {
 		initiallyOpen.push($(this).attr("id"));
