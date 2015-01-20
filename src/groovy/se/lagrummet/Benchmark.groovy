79e18eb6d720580e3abd4a5293f59c32c58b8c53
cheijken <christian.heijkenskjold@gmail.com>
2015-01-20 14:46:31 +0100

    Benchmark page render. #238

--- src/groovy/se/lagrummet/Benchmark.groovy
+++ src/groovy/se/lagrummet/Benchmark.groovy
@@ -1,5 +1,7 @@
 package se.lagrummet
 
+import groovy.time.TimeCategory
+
 /**
  * Created by christian on 1/19/15.
  * Encapsulate a section of code you want to benchmark and print to log
@@ -9,9 +11,32 @@ package se.lagrummet
 class Benchmark {
 
     static def section = { logText, log, body ->
-        def timestamp = System.currentTimeMillis()
+        def timestamp = new Date()
         body.call()
-        def elapsedTime = System.currentTimeMillis() - timestamp
-        log.info("${logText} ${elapsedTime} ms")
+        log.info(logText+" "+TimeCategory.minus(new Date(), timestamp))
+    }
+
+    static def sectionByIntermediate = { logText, log, body ->
+        def intermediate = new Intermediate(log)
+        body.call(intermediate)
+        intermediate.sum(logText)
+    }
+
+    static class Intermediate {
+        def log
+        def timestamp = new Date()
+        def list = [:]
+
+        Intermediate(log) {
+            this.log = log
+        }
+
+        void log(String comment) {
+            list.put(new Date(), comment)
+        }
+        void sum(String text) {
+            list.each { start, comment -> log.info(comment+" "+TimeCategory.minus( (Date) start, timestamp)) }
+            log.info(text+" "+TimeCategory.minus(new Date(), timestamp))
+        }
     }
 }
