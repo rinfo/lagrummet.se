9dcf4cb6960f9637c2302a49d2012697691a3cb8
Kåre Jonsson <kare.jonsson@dom.se>
2015-01-26 16:17:21 +0100

    Corrects user id retrieval

--- grails-app/controllers/se/lagrummet/ProfileController.groovy
+++ grails-app/controllers/se/lagrummet/ProfileController.groovy
@@ -8,25 +8,21 @@ class ProfileController {
 	def springSecurityService
 	
 	def edit = {
-        def userInstance = (params.id) ? User.get(params.id) : User.get(springSecurityService.principal.id)
-        //def userInstance = User.get(params.id)
+        def userInstance = User.get(springSecurityService.principal.id)
+
         if (!userInstance) {
-			//println "ProfileController.profile true, params="+params
             flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'user.label', default: 'User'), params.id])}"
             redirect(action: "list")
         }
         else {
-			//println "ProfileController.profile false, params="+params
             [userInstance: userInstance]
         }
 	}
 	
 	def updateProfile = {
-		//println "ProfileController.updateProfile params="+params
-        User userInstanceOld = (params.id) ? User.get(params.id) : User.get(springSecurityService.principal.id)
-        //def userInstance = User.get(params.id)
-        if (!userInstanceOld) {
-			//println "ProfileController.profile true, params="+params
+        User userInstanceOld = User.get(springSecurityService.principal.id)
+
+		        if (!userInstanceOld) {
             flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'user.label', default: 'User'), params.id])}"
             redirect(action: "list")
 			return
@@ -39,7 +35,8 @@ class ProfileController {
 	}
 	
 	def show = {
-        def userInstance = (params.id) ? User.get(params.id) : User.get(springSecurityService.principal.id)
+        def userInstance = User.get(springSecurityService.principal.id)
+		
 		if (!userInstance) {
 			flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'user.label', default: 'User'), params.id])}"
 			redirect(action: "list")
@@ -50,43 +47,33 @@ class ProfileController {
 	}
 	
 	def credentials = {
-		//println "ProfileController.credentials, params="+params
 	}
 
 	def updateCredentials = {
-		//println "ProfileController.updateCredentials, params="+params
 		String encryptedPassword = springSecurityService.encodePassword(params.currentpassword)
-		//println "ProfileController.updateCredentials, krypterat lösen = "+encryptedPassword
 		
-        def userInstance = (params.id) ? User.get(params.id) : User.get(springSecurityService.principal.id)
+        def userInstance = User.get(springSecurityService.principal.id)
 		if (!userInstance) {
 			flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'user.label', default: 'User'), params.id])}"
 			redirect(action: "show")
 		}
 
-		//println "ProfileController.updateCredentials, tidigare sparat krypterat lösen = "+userInstance.password
-		
 		if(encryptedPassword.compareTo(userInstance.password) == 0) {
-			//println "ProfileController.updateCredentials, användaren anses ha skrivit rätt lösenord"
 		}
 		else {
-			//println "ProfileController.updateCredentials, fel lösenord från användaren."
 			flash.message = "${message(code: 'user.password.wrong.label', args: [message(code: 'user.label', default: 'User'), params.id])}"
 			redirect(action: "credentials")
 			return;
 		}
 		
 		if(params.newpassword.compareTo(params.newrepeatedpassword) == 0) {
-			//println "ProfileController.updateCredentials, användaren anses ha skrivit samma nya lösenord"
 		}
 		else {
-			//println "ProfileController.updateCredentials, fel nytt lösenord från användaren. Olika vid repetition."
 			flash.message = "${message(code: 'user.password.wrongrepetition.label', args: [message(code: 'user.label', default: 'User'), params.id])}"
 			redirect(action: "credentials")
 			return;
 		}
 
-		//println "ProfileController.updateCredentials, bra skriv i databasen."
 		userInstance.password = springSecurityService.encodePassword(params.newpassword)
 		
 		userInstance.save(flush: true)
@@ -94,11 +81,5 @@ class ProfileController {
 
 		
 	}
-//65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5
-//1234567890123456789012345678901234567890123456789012345678901234567890
-	/*
-ProfileController.updateCredentials, params=[update:Update, newrepeatedpassword:c, currentpassword:a, 
-SYNCHRONIZER_TOKEN:dfbff317-309b-46c5-a371-07f8cae4fc1b, SYNCHRONIZER_URI:/lagrummet.se/profile/credentials, 
-newpassword:b, action:updateCredentials, controller:profile]
-	 */
+
 }
