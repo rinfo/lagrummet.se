e9c1f602abff0bdeaaa8b2e4f711fd6ca80439af
cheijken <christian.heijkenskjold@gmail.com>
2015-02-23 15:19:34 +0100

    Fix expirex calc. #321

--- web-app/js/application.js
+++ web-app/js/application.js
@@ -357,8 +357,8 @@ function getCookie(cname) {
 
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    d.setTime(d.getTime() + (exdays));
     var expires = "; expires="+d.toUTCString();
-    var maxAge = "; max-age="+d.toUTCString();
+    var maxAge = "; max-age="+(exdays*24*60*60*1000);
     document.cookie = cname + "=" + cvalue + expires + maxAge + "; path=/;";
 }
