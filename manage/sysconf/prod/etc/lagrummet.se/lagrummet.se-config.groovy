2bc4ec925b8517f35ee728967e6274c1453f1f68
Minh Nguyen <xqs.minh@gmail.com>
2015-10-23 14:18:10 +0200

    Update config file for prod server (configured as www.lagrummet.se)

--- manage/sysconf/prod/etc/lagrummet.se/lagrummet.se-config.groovy
+++ manage/sysconf/prod/etc/lagrummet.se/lagrummet.se-config.groovy
@@ -1,14 +1,28 @@
-grails.serverURL = "http://beta.lagrummet.se"
+grails.serverURL = "http://www.lagrummet.se"
 lagrummet.local.rinfo.view = "${grails.serverURL}/rinfo/"
 
-lagrummet.rdl.service.baseurl="http://service.lagrummet.se/"
-lagrummet.rdl.rinfo.baseurl="http://rinfo.lagrummet.se/"
-lagrummet.onlyLocalSearch = true
+// Route rinfo-service requests through varnish
+// TODO: Varnish in production?
+//lagrummet.rdl.service.baseurl="http://127.0.0.1:8383/"
+
+// Setup search interface (RDL)
+//lagrummet.rdl.service.baseurl="http://service.stage.lagrummet.se/"
+//lagrummet.rdl.rinfo.baseurl="http://rinfo.stage.lagrummet.se/"
+
+// Uncomment these to display start page without search
 lagrummet.mainLayoutName = "mainSimpleSearch"
 lagrummet.mainEnglishLayoutName = "mainSimpleSearch"
 
+// Use local mysql database on target server
 dataSource.url = "jdbc:mysql://127.0.0.1:3306/lagrummet"
 dataSource.username = "lagrummet2"
-dataSource.password = "<CHANGEME>"
+dataSource.password = "changeme"
 
-lagrummet.googleAnalytics.webPropertyId = "UA-10527860-1"
+lagrummet.googleTagManager.webPropertyId = "GTM-NKM2G5"
+
+// TODO: ip restrictions for admin in production?
+//grails.plugins.springsecurity.ipRestrictions = [
+//        '/admin/**': [
+//                '159.190.251.56', '159.190.251.57', '159.190.240.67',
+//        ]
+//]
